e8b3fee584874f0113aa2dce4ff88178d19b7676
mahdi paykoub <mahdi.paykoub.github@gmail.com>
2025-09-03 10:10:09 -0800

    add customers box

--- js/js.js
+++ js/js.js
@@ -11,3 +11,22 @@
       clickable: true,
     },
   });
+
+  var swiper = new Swiper(".mySwiper_2", {
+    slidesPerView: 4,
+    spaceBetween: 20,
+    loop: true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    breakpoints: {
+      320: { slidesPerView: 1 },
+      768: { slidesPerView: 2 },
+      1024: { slidesPerView: 4 },
+    },
+  });
