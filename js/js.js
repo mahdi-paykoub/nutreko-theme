9c1a2e94f30dd64e11fe6f15550eedde2fe116af
mahdi paykoub <mahdi.paykoub.github@gmail.com>
2025-09-03 10:27:08 -0800

    add brands

--- js/js.js
+++ js/js.js
@@ -12,7 +12,7 @@
     },
   });
 
-  var swiper = new Swiper(".mySwiper_2", {
+  var mySwiper_2 = new Swiper(".mySwiper_2", {
     slidesPerView: 4,
     spaceBetween: 20,
     loop: true,
@@ -28,5 +28,16 @@
       320: { slidesPerView: 1 },
       768: { slidesPerView: 2 },
       1024: { slidesPerView: 4 },
+    },
+  });
+
+   const brand_s = new Swiper('.brand_s', {
+    slidesPerView: 5,      // تعداد تصاویر در یک ردیف
+    spaceBetween: 10,      // فاصله بین تصاویر
+    loop: true,            // حرکت بی‌نهایت
+    speed: 5000,           // سرعت حرکت (میلی‌ثانیه)
+    autoplay: {
+      delay: 0,            // بدون توقف
+      disableOnInteraction: false,
     },
   });
